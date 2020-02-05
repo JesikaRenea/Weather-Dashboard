08ec7ab3ff3398a2259752e96e6b6c5366ca8136
JesikaRenea <JessicaHayesUCF@outlook.com>
2020-02-04 20:59:42 -0500

    Bad Request Issues

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ var myApi = "54448964d77673d05e876660501678d0";
 var cityName = $("#city-name").val().trim();
 var searchBtn = $(".search-button");
 
-var queryUrl = "https://api.openweathermap.org/data/2.5/weather?&APPID=54448964d77673d05e876660501678d0";
+var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=54448964d77673d05e876660501678d0";
 var queryFiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&APPID=54448964d77673d05e876660501678d0";
-
-queryUrl += "&q=" + cityName;
 
 searchBtn.on("click", function(event){
     event.preventDefault();
