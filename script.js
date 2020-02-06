40e71e22a8c8a687e5bd1f2a23087f306a5bd0d2
JesikaRenea <JessicaHayesUCF@outlook.com>
2020-02-06 14:29:23 -0500

    Working API for City Forecast

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
+(function () {
+    var NowMoment = moment().format('MMMM Do YYYY, h:mm a');
+    var displayMoment = document.getElementById('currentDay');
+    displayMoment.innerHTML = NowMoment;
+})();
+
+
+
+
 var myApi = "54448964d77673d05e876660501678d0";
 
-var cityName = $("#city-name").val().trim();
 var searchBtn = $(".search-button");
 
-var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=54448964d77673d05e876660501678d0";
-var queryFiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&APPID=54448964d77673d05e876660501678d0";
+// var queryFiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&APPID=54448964d77673d05e876660501678d0";
 
 searchBtn.on("click", function(event){
     event.preventDefault();
     console.log("click");
 
+    var cityName = $("#city-name").val().trim();
+    var queryWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=54448964d77673d05e876660501678d0&units=imperial";
+    var queryFiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&APPID=54448964d77673d05e876660501678d0units=imperial";
+
+
     $.ajax({
-        url: queryUrl,
+        url: queryWeatherUrl,
         method: 'GET'
     })
         .then(function (response) {
-            console.log(url);
+            console.log(queryWeatherUrl);
             console.log(response)
         });
 
