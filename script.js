582ecb8dc678784f1decb14d2b6c20849152a973
JesikaRenea <JessicaHayesUCF@outlook.com>
2020-02-08 13:31:14 -0500

    Stores recent searches

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ var myApi = "54448964d77673d05e876660501678d0";
 
 var searchBtn = $(".search-button");
 
+var city = [];
+
 // var queryFiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&APPID=54448964d77673d05e876660501678d0";
 
 searchBtn.on("click", function (event) {
@@ -19,21 +21,42 @@ searchBtn.on("click", function (event) {
     var cityName = $("#city-name").val().trim();
     var queryWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=54448964d77673d05e876660501678d0&units=imperial";
     var queryFiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&APPID=54448964d77673d05e876660501678d0&units=imperial";
+    var queryCityUv = "http://api.openweathermap.org/data/2.5/uvi/forecast?&APPID=54448964d77673d05e876660501678d0&units=imperial&lat={lat}&lon={lon}&cnt={cnt}";
+    renderCity(cityName);
+
+    function renderCity(newcity) {
+        city.push(newcity);
+        $("#recent-search").empty();
+        for (var i = 0; i < city.length; i++) {
+            var newLi = $("<li>");
+            newLi.addClass("list-group-item");
+            newLi.attr("data-city", city[i]);
+            newLi.text(city[i]);
+            $("#recent-search").append(newLi);
+        }
+    }
+
+
+
 
     $.ajax({
         url: queryWeatherUrl,
         method: 'GET'
     })
         .then(function (response) {
-            // // console.log(queryWeatherUrl);
-            // // console.log(response);
-            // console.log("City: " + response.name);
-            // console.log(response.weather[0].description);
-            // console.log("Temperature: " + response.main.temp + "F");
-            // console.log("Humidity: " + response.main.humidity + "%");
-            // console.log("Windspeed: " + response.wind.speed + "MPH");
-            var city = response.name;
+            $(".forecast").empty();
+            console.log(queryWeatherUrl);
+            console.log(response);
+            
+            console.log(response.coord.lon)
+            console.log(response.coord.lat)
 
+            console.log("City: " + response.name);
+            console.log(response.weather[0].description);
+            console.log("Temperature: " + response.main.temp + "F");
+            console.log("Humidity: " + response.main.humidity + "%");
+            console.log("Windspeed: " + response.wind.speed + "MPH");
+            var city = response.name;
 
             var cityWeatherDiv = $("<div>");
 
@@ -57,6 +80,8 @@ searchBtn.on("click", function (event) {
             console.log(queryWeatherUrl);
             console.log(response);
             console.log(response.list)
+            $("#fiveday-div").empty();
+
 
             var fiveDay = response.list;
 
@@ -77,10 +102,9 @@ searchBtn.on("click", function (event) {
                 var fiveIconImg = $("<p>").text(fiveDay[i].weather[0].description);
                 var fiveTempP = $("<p>").text("Temperature: " + fiveDay[i].main.temp + " F");
                 var fiveHumidP = $("<p>").text("Humidity: " + fiveDay[i].main.humidity + "%");
-    
+
                 fiveDayDiv.append(fiveDayP, fiveIconImg, fiveTempP, fiveHumidP);
                 $("#fiveday-div").append(fiveDayDiv);
-
 
             }
 
